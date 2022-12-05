f7d9b87046fa56b02208b31d1314ea018bd1dd53
MROJ4N <66457417+mroj4n@users.noreply.github.com>
2022-12-05 15:02:40 +0100

    recorder depthnp

--- recorder.py
+++ recorder.py
@@ -31,7 +31,7 @@ i=0
 main_folder_name=main_folder_name+str(i)
 while os.path.exists(main_folder_name+"/"):
     i=i+1
-    main_folder_name[-1]=str(i)
+    #main_folder_name[-1]=str(i)
 main_folder_name=main_folder_name+"/"
 Spark_filename= main_folder_name+"Spark_image/"
 Spark_numpys=main_folder_name+"Spark_npys/"
@@ -40,6 +40,7 @@ Spark_maxs=main_folder_name+"Spark_maxs/"
 
 Color_filename= main_folder_name+"Color_RealSense/"
 Depth_filename= main_folder_name+"Depth_RealSense/"
+Depthnp_filename = main_folder_name+"Depthnp_RealSense/"
 file_ext = ".jpg"
 counter=0
 
@@ -52,7 +53,7 @@ os.mkdir(Spark_mins)
 os.mkdir(Spark_maxs)
 os.mkdir(Color_filename)
 os.mkdir(Depth_filename)
-
+os.mkdir(Depthnp_filename)
 np.save(main_folder_name+"depth_scale.npy",depth_scale)
 while True:
     grideye_image = ge.GetGridValue()
@@ -66,7 +67,8 @@ while True:
     cv2.imwrite(Spark_filename+str(counter)+file_ext, grideye_image)
     cv2.imwrite(Color_filename+str(counter)+file_ext, color_image)
     cv2.imwrite(Depth_filename+str(counter)+file_ext, depth_map)
-    cv2.imshow("RealSense image", color_image)
+    np.save(Depthnp_filename+str(counter)+".npy",depth_map)
+    cv2.imshow("RealSense image", depth_map)
     counter=counter+1
     k = cv2.waitKey(1)
     if k == 27:
